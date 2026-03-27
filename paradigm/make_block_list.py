0c623398618d0239f75aa96f08663c511a1aa5c9
Kornelia Renachowska <renachowskakornelia@gmail.com>
2026-03-27 15:20:36 +0100

    Randomization final

--- paradigm/make_block_list.py
+++ paradigm/make_block_list.py
@@ -2,25 +2,26 @@
 import numpy as np
 import pandas as pd
 
+
 # %%
 
 # %%
 def generate_blocks(pid):
- np.random.seed(pid)
- melodies = list(range(1, 31))
- # losowo wybieramy wersję dla każdego sid (15/15)
- harmonicity = np.random.permutation(['harm']*15 + ['inh']*15)
- df = pd.DataFrame({
- 'sid': melodies,
- 'harmonicity': harmonicity
- })
- df['harmonicity_trigs'] = df['harmonicity'].map({'inh': 100, 'harm': 0})
- df['pid'] = pid
- df['trig'] = df['harmonicity_trigs'] + df['sid']
- df['filename'] = df['harmonicity'] + "_" + df['sid'].astype(str) + ".wav"
- df = df.sample(frac=1).reset_index(drop=True)
+    melodies = list(range(1, 31))
+    # losowo wybieramy wersję dla każdego sid (15/15)
+    harmonicity = np.random.permutation(['harm'] * 15 + ['inh'] * 15)
+    df = pd.DataFrame({
+        'sid': melodies,
+        'harmonicity': harmonicity
+    })
+    df['harmonicity_trigs'] = df['harmonicity'].map({'inh': 100, 'harm': 0})
+    df['pid'] = pid
+    df['trig'] = df['harmonicity_trigs'] + df['sid']
+    df['filename'] = df['harmonicity'] + "_" + df['sid'].astype(str) + ".wav"
+    df = df.sample(frac=1).reset_index(drop=True)
 
- return df
+    return df
+
 
 # %%
 df = generate_blocks(1)
@@ -28,3 +29,4 @@ df = generate_blocks(1)
 for p in range(60):
     d = generate_blocks(p)
     d.to_csv(f'soundpool/p{p}_blocks.csv')
+
